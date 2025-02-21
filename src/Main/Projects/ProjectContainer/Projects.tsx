68884ead18709d3252bb7da8a93783644db8d332
sang53 <143582855+sang53@users.noreply.github.com>
2025-02-22 05:44:18 +1100

    add project styling, link hover animation

--- src/Main/Projects/ProjectContainer/Projects.tsx
+++ src/Main/Projects/ProjectContainer/Projects.tsx
@@ -5,6 +5,7 @@ import classes from "./Projects.module.css";
 export default function Projects() {
   return (
     <section id="projects-container" className={classes.projectContainer}>
+      <h1>Projects</h1>
       {projects.map((project, idx) => {
         return <ProjectItem key={idx} project={project} />;
       })}
